0c4dccfa8e4a8b81cc1569349babf4f594d9dfda
Yurii Sokolovskyi <s-y-r@ukr.net>
2019-09-11 22:30:06 +0300

    first homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert("hello");
+'use strict';
+
+let money,
+    time;
+
+money = prompt("Ваш бюджет на месяц?","0");
+time = prompt("Ваш бюджет на месяц?","YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income:[],
+    savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    appData.expenses[prompt("Введите обязательную статью расходов в этом месяце","")] = prompt("Во сколько обойдется?","0");
+}
+
+alert(appData.budget/30);
+
+console.log(appData.expenses);
+console.log(time);
